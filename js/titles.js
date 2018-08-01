819ce4bd741b60846400b0dd7a277919707819c3
Remy Aubrun <remy.aubrun27@gmail.com>
2018-08-01 12:46:38 -0400

    edits to title html,css,js and roster css

--- js/titles.js
+++ js/titles.js
@@ -1,16 +1,39 @@
-$(document).ready(function(){
-	$(".title").children('ul').hide();
-	
-		$('.clickable1').hover(function(){
-			$(this).next().stop(true, true);
-			$(this).next().slideDown('fast'); 
-		},
-		function(){
-			$(this).next().stop(true, true);
-			$(this).next().slideUp('slow');
+var config = {
+    apiKey: "AIzaSyAbx0OXAVhGn01Tp8OGww5YSosbSLMKxmg",
+    authDomain: "manchestercity-7b95a.firebaseapp.com",
+    databaseURL: "https://manchestercity-7b95a.firebaseio.com",
+    projectId: "manchestercity-7b95a",
+    storageBucket: "manchestercity-7b95a.appspot.com",
+    messagingSenderId: "592524579818"
+  };
+  var app = firebase.initializeApp(config);
+  var db = firebase.firestore(app);
+
+//    db.collection("titles").doc("fa").get().then(function(doc){
+// 	if(doc.exists){
+// 		console.log(doc.data());
+// 	}
+//   })
+
+db.collection("titles").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+		// doc.data() is never undefined for query doc snapshots
+		if(doc.id === "euro"){
+			console.log(doc.id, " => ", doc.data());
 		}
-	
-	);
-	
-	
+    });
 });
+  //console.log(title);
+
+
+//   db.collection("titles").where("capital", "==", true)
+//     .get()
+//     .then(function(querySnapshot) {
+//         querySnapshot.forEach(function(doc) {
+//             // doc.data() is never undefined for query doc snapshots
+//             console.log(doc.id, " => ", doc.data());
+//         });
+//     })
+//     .catch(function(error) {
+//         console.log("Error getting documents: ", error);
+//     });
